327b4fcbd1cd904762a8054a6f7166791030a123
Rendrikson Oliveira <40209027+Rendrikson98@users.noreply.github.com>
2020-09-15 16:46:14 -0300

    escopo_site

---
+++ tamplate/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Inicial from './Components/TelaInicial/telaInicial';
+
+class App extends Component {
+  render() {
+    return (
+        <Inicial />
+    );
+  }
+}
+
+export default App;
---
+++ tamplate/src/Components/TelaInicial/telaInicial.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './style.css';
+
+export default function Inicial(){
+    return(
+        <div className='bodyMaster'>
+            <div className='menu'>
+
+            </div>
+            <div className='body'>
+                <div className='couteudo'>
+                    <div className='conteudo1'>
+            
+                    </div>
+                    <div className='conteudo2'>
+
+                    </div>
+                </div>
+                <div className='anuncio'>
+
+                </div>
+            </div>
+        </div>
+    )
+}
